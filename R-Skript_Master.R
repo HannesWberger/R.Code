a61646c56a7c9a08f3005bd2765bb5cd9746819a
HannesWberger <145850479+HannesWberger@users.noreply.github.com>
2023-09-23 15:33:56 +0200

    Update R-Skript_Master.R

--- R-Skript_Master.R
+++ R-Skript_Master.R
@@ -1,43 +1,9 @@
-getwd()
-setwd("/Users/hanneswirnsberger/Documents/Master_Thesis/R_thesis")
-getwd()
-
-#Datensätze einlesen
 biomass <- read.csv("/Users/hanneswirnsberger/Documents/Master_Thesis/Data/daten_geprueft/Biomasse.csv",
                     header = TRUE, sep =";", dec = ",", na ="NA")
 values <- read.csv("/Users/hanneswirnsberger/Documents/Master_Thesis/Finale Datensätze/Biomasse_Erosion.csv",
                    header = TRUE, sep =";", dec = ",", na ="NA")
-rusle <- read.csv("/Users/hanneswirnsberger/Documents/Master_Thesis/Data/daten_geprueft/20230512_RUSLE_veg.csv",
-                  header = TRUE, sep =",", dec = ".", na ="NA")
 erosion <- read.csv("/Users/hanneswirnsberger/Documents/Master_Thesis/Finale Datensätze/Erosionswannen_Werte.csv",
                     header = TRUE, sep =";", dec = ",", na ="NA")
-
-attach(biomass)
-biomass_01 <- subset (biomass, site==1)
-biomass_02 <- subset (biomass, site==2)
-biomass_03 <- subset (biomass, site==3)
-biomass_04 <- subset (biomass, site==4)
-
-#Datensatz ohne Extremwerte bei AGB
-#data_filter <- subset (data, id!="s4_p04" & id!="s4_p07" & id!="s4_p10" & id!="s4_p11")
-#hang_filter <- subset (hang, id!="P4_04" & id!="P4_07" & id!="P4_10" & id!="P4_11")
-
-library(ggplot2)
-library(stargazer)
-library(ggpubr)
-library (EnvStats)
-library(ggstatsplot)
-
-#Statistischer Unterschied der AGB zwischen den verschiedenen Sukzesssionszonen
-
-#prüfung auf Normalverteilung
-qqnorm(no_outliers_agb01$adj_agb)
-hist(no_outliers_agb01$adj_agb)
-
-#Wilcox-test Prüfung
-wilcox.test(biomass_02$adj_agb~biomass_03$adj_agb)
-
-#Boxplots, AGB with Moss, Adjusted AGB
 
 my_comparisons <- list(c("1","2"),c("2","3"),c("3","4"))
 
@@ -51,7 +17,7 @@ ggboxplot(biomass, x = "site", y = "agb_g_m2", fill = "#CCCCCC", outlier.shape =
   stat_compare_means(comparisons = my_comparisons, method = "wilcox.test", size = 6)+
   theme(text = element_text(size = 25))
 
-#Boxplot Erosionswannen
+#Boxplot Erosion
 
 ggboxplot(erosion, x = "zone", y = "ssy", fill = "#CCCCCC", outlier.shape = NA,
           bxp.errorbar = TRUE, bxp.errorbar.width = 0.5, outlier.size = NA, 
@@ -63,41 +29,7 @@ ggboxplot(erosion, x = "zone", y = "ssy", fill = "#CCCCCC", outlier.shape = NA,
   stat_compare_means(comparisons = my_comparisons, method = "wilcox.test", size = 6)+
   theme(text = element_text(size = 25))
 
-#Boxplot mit T-test EROSION
-
-#Ausreißer in RUSLE entfernen
-#Finden Sie Q1, Q3 und den Interquartilsabstand für Werte in Spalte A.
-Q1 <- quantile(rusle$log, .25, na.rm = TRUE)
-Q3 <- quantile(rusle$log, .75, na.rm = TRUE)
-IQR <- IQR(rusle$log, na.rm = TRUE)
-
-# Behalten Sie nur Zeilen im Dataframe bei, deren Werte innerhalb von 1,5 * IQR von Q1 und Q3 liegen
-no_outliers <- subset(rusle, rusle$log> (Q1 - 1.5*IQR) & rusle$log< (Q3 + 1.5*IQR))
-
-#sortieren
-rusle <- no_outliers[order(no_outliers$layer),]
-
-#z-standardisieren
-rusle$ZDN <- scale(rusle$DN)
-
-#Histrogramm für Darstellung der Verteilung
-hist(rusle$log)
-hist(biomass$adj_agb_log)
-shapiro.test(rusle$log)
-biomass$adj_agb_log <- log10(biomass$adj_agb)
-
-#Boxplots mit logarithmierten Werten
-my_comparisons_rusle <- list(c("Zone 1","Zone 2"),c("Zone 2","Zone 3"),c("Zone 3","Zone 4"))
-
-ggboxplot(rusle, x = "layer", y = "log", fill = "#CCCCCC",
-          bxp.errorbar = TRUE, bxp.errorbar.width = 0.5, 
-          xlab = "Sites", ylab = "Soil Erosion (log)")+
-  stat_n_text(y.pos = -0.5, size = 6)+
-  stat_median_iqr_text(y.pos = 0.075, size = 6)+
-  stat_compare_means(comparisons = my_comparisons_rusle, method = "wilcox.test", size = 6)+
-  theme(text = element_text(size = 25))
-
-#Vergleich der Mediane AGB
+#Comparison AGB and Erosion
 
 ggplot(values, aes(y = agb_mean, group = zone, x = erosion_mean, label = zone_name, shape = zone_name))+
   geom_point(size = 5)+
@@ -114,18 +46,3 @@ ggplot(values, aes(y = agb_mean, group = zone, x = erosion_mean, label = zone_na
   labs(y = expression("Mean AGB in g/m"^-2))+
   labs(shape='')+
   theme(text = element_text(size = 35))
-
-
-sd(biomass_01$adj_agb)
-sd(biomass_02$adj_agb)
-sd(biomass_03$adj_agb)
-sd(biomass_04$adj_agb)
-
-#Lineare Regression
-plot(biomass$agb_g_m2,biomass$E_median)
-
-model <- lm(biomass$E_median~biomass$agb_g_m2)
-
-abline(model)
-
-summary(model)
